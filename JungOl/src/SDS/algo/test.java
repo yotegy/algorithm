22846cd98aaad4975625b26068287e67c0524a0b
timjoo <yotegy@gmail.com>
2016-09-25 22:38:57 +0900

    fixed untracked files

---
+++ JungOl/src/SDS/algo/test.java
@@ -0,0 +1,16 @@
+package SDS.algo;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String num = "486";
+		String num2 = "48631253";
+		
+		System.out.println(num2.indexOf(num));
+		
+
+	}
+
+}
